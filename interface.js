045101e154dde2707039e84891d00542944e0b92
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-13 14:13:35 +0300

    Update interface.js

--- interface.js
+++ interface.js
@@ -14,13 +14,15 @@
     var modules = [
         'https://mastermagic98.github.io/interface/color.js',
         'https://mastermagic98.github.io/interface/aloader.js'
-        'https://mastermagic98.github.io/interface/animations.js',
-        'https://mastermagic98.github.io/interface/translate_tv.js',
-        'https://mastermagic98.github.io/interface/bigbuttons.js',
-        'https://mastermagic98.github.io/interface/incardtemplate.js',
-        'https://mastermagic98.github.io/interface/forall.js',
-        'https://mastermagic98.github.io/interface/fix_lang.js'
     ];
+
+    // Додайте інші модулі за потреби:
+    // 'https://mastermagic98.github.io/interface/animations.js'
+    // 'https://mastermagic98.github.io/interface/translate_tv.js'
+    // 'https://mastermagic98.github.io/interface/bigbuttons.js'
+    // 'https://mastermagic98.github.io/interface/incardtemplate.js'
+    // 'https://mastermagic98.github.io/interface/forall.js'
+    // 'https://mastermagic98.github.io/interface/fix_lang.js'
 
     // Підключаємо модулі
     modules.forEach(function (url) {
