7bf7a7b399b621996242e971b92f1bf2fd2b3411
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-08-13 14:31:25 +0300

    Update color.js

--- color.js
+++ color.js
@@ -1,21 +1,27 @@
 (function () {
     'use strict';
 
+    // Додаємо переклади
     Lampa.Lang.add({
         accent_color: {
-            ru: 'Колір акценту',
+            ru: 'Цвет акцента',
             en: 'Accent color',
             uk: 'Колір акценту'
         }
     });
 
     function applyAccentColor(hue) {
-        var color = 'hsl(' + hue + ', 60%, 50%)'; // Фіксовані Saturation і Lightness
+        var color = 'hsl(' + hue + ', 60%, 50%)';
         document.documentElement.style.setProperty('--main-color', color);
         Lampa.Storage.set('accent_color_selected', color);
+        // Викликаємо подію для оновлення анімації
+        var event = new Event('style-change');
+        document.documentElement.dispatchEvent(event);
+        console.log('Колір змінено на: ' + color); // Діагностика
     }
 
     function initColorPicker() {
+        console.log('Ініціалізація color.js'); // Діагностика
         Lampa.SettingsApi.addParam({
             component: 'accent_color_plugin',
             param: {
@@ -31,18 +37,21 @@
                 description: '<div style="width: 2em; height: 2em; background-color: ' + Lampa.Storage.get('accent_color_selected', '#5daa68') + '; display: inline-block;"></div>'
             },
             onChange: function (value) {
+                console.log('Обрано відтінок: ' + value); // Діагностика
                 applyAccentColor(value);
                 Lampa.Settings.update();
             },
             onRender: function (item) {
                 var color = Lampa.Storage.get('accent_color_selected', '#5daa68');
                 item.find('.settings-param__descr div').css('background-color', color);
+                console.log('Рендеринг повзунка кольору, поточний колір: ' + color); // Діагностика
             }
         });
 
         // Застосовуємо збережений колір
         var savedColor = Lampa.Storage.get('accent_color_selected', '#5daa68');
         document.documentElement.style.setProperty('--main-color', savedColor);
+        console.log('Застосовано збережений колір: ' + savedColor); // Діагностика
     }
 
     if (window.appready) {
@@ -50,6 +59,7 @@
     } else {
         Lampa.Listener.follow('app', function (event) {
             if (event.type === 'ready') {
+                console.log('Lampa готова, ініціалізація color.js'); // Діагностика
                 initColorPicker();
             }
         });
