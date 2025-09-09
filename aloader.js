d542751c6b832e54910ed26a3f66223685ec5b39
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-09-09 18:57:17 +0300

    Update aloader.js

--- aloader.js
+++ aloader.js
@@ -89,10 +89,9 @@
         var style = document.createElement('style');
         style.id = 'aniload';
         var mainColor = Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ? Lampa.Storage.get('color_plugin_main_color', '#ffffff') : '#ffffff';
+        var focusColor = Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ? Lampa.Storage.get('color_plugin_main_color', '#ffffff') : '#ffffff';
         var filterValue = 'url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22color%22 color-interpolation-filters=%22sRGB%22%3E%3CfeColorMatrix type=%22matrix%22 values=%220 0 0 0 ' + hexToRgb(mainColor).r/255 + ' 0 0 0 0 ' + hexToRgb(mainColor).g/255 + ' 0 0 0 0 ' + hexToRgb(mainColor).b/255 + ' 0 0 0 1 0%22/%3E%3C/filter%3E%3C/svg%3E#color")';
-        var focusFilterValue = Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ?
-            'url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22focus_color%22 color-interpolation-filters=%22sRGB%22%3E%3CfeColorMatrix type=%22matrix%22 values=%220 0 0 0 ' + hexToRgb(mainColor).r/255 + ' 0 0 0 0 ' + hexToRgb(mainColor).g/255 + ' 0 0 0 0 ' + hexToRgb(mainColor).b/255 + ' 0 0 0 1 0%22/%3E%3C/filter%3E%3C/svg%3E#focus_color")' :
-            'url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22focus_color%22 color-interpolation-filters=%22sRGB%22%3E%3CfeColorMatrix type=%22matrix%22 values=%220 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0%22/%3E%3C/filter%3E%3C/svg%3E#focus_color")';
+        var focusFilterValue = 'url("data:image/svg+xml,%3Csvg xmlns=%22http://www.w3.org/2000/svg%22%3E%3Cfilter id=%22focus_color%22 color-interpolation-filters=%22sRGB%22%3E%3CfeColorMatrix type=%22matrix%22 values=%220 0 0 0 ' + hexToRgb(focusColor).r/255 + ' 0 0 0 0 ' + hexToRgb(focusColor).g/255 + ' 0 0 0 0 ' + hexToRgb(focusColor).b/255 + ' 0 0 0 1 0%22/%3E%3C/filter%3E%3C/svg%3E#focus_color")';
         style.textContent = '.ani_modal_root { padding: 1em; }' +
                             '.ani_picker_container { display: grid; grid-template-columns: 1fr 1fr; gap: 140px; padding: 0; }' +
                             '.ani_picker_container > div { display: flex; flex-direction: column; align-items: center; }' +
@@ -100,12 +99,12 @@
                             '.ani_loader_row { display: flex; flex-wrap: wrap; gap: 30px; margin-bottom: 10px; justify-content: center; }' +
                             '.ani_loader_square { width: 35px; height: 35px; border-radius: 4px; display: flex; flex-direction: column; justify-content: center; align-items: center; cursor: pointer; color: #ffffff !important; font-size: 10px; text-align: center; }' +
                             '.ani_loader_square img { max-width: 30px; max-height: 30px; object-fit: contain; filter: ' + filterValue + '; }' +
-                            '.ani_loader_square.focus { border: 0.3em solid ' + (Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ? 'var(--main-color)' : '#ffffff') + '; transform: scale(1.1); }' +
+                            '.ani_loader_square.focus { border: 0.3em solid ' + focusColor + '; transform: scale(1.1); }' +
                             '.ani_loader_square.focus img { filter: ' + focusFilterValue + '; }' +
                             '.ani_loader_square.default { width: 35px; height: 35px; border-radius: 4px; background-image: url(./img/loader.svg); background-size: contain; background-repeat: no-repeat; background-position: center; filter: ' + filterValue + '; }' +
-                            '.ani_loader_square.default.focus { border: 0.3em solid ' + (Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ? 'var(--main-color)' : '#ffffff') + '; transform: scale(1.1); filter: ' + focusFilterValue + '; }' +
+                            '.ani_loader_square.default.focus { border: 0.3em solid ' + focusColor + '; transform: scale(1.1); filter: ' + focusFilterValue + '; }' +
                             '.svg_input { width: 410px; height: 35px; border-radius: 8px; border: 2px solid #ddd; position: relative; cursor: pointer; display: flex; flex-direction: column; align-items: center; justify-content: center; color: #fff !important; font-size: 12px; font-weight: bold; text-shadow: 0 0 2px #000; background-color: #353535; }' +
-                            '.svg_input.focus { border: 0.3em solid ' + (Lampa.Storage.get('color_plugin_enabled', 'true') === 'true' ? 'var(--main-color)' : '#ffffff') + '; transform: scale(1.1); }' +
+                            '.svg_input.focus { border: 0.3em solid ' + focusColor + '; transform: scale(1.1); }' +
                             '.svg_input .label { position: absolute; top: 1px; font-size: 10px; }' +
                             '.svg_input .value { position: absolute; bottom: 1px; font-size: 10px; }';
         document.head.appendChild(style);
@@ -347,11 +346,12 @@
         // Слухач зміни кольору та стану плагіна color.js
         Lampa.Storage.listener.follow('change', function (e) {
             if (e.name === 'color_plugin_main_color' || e.name === 'color_plugin_enabled') {
+                // Оновлюємо кастомний завантажувач
                 if (Lampa.Storage.get('ani_load') && Lampa.Storage.get('ani_active') && Lampa.Storage.get('ani_load') !== './img/loader.svg') {
                     setCustomLoader(Lampa.Storage.get('ani_load'));
                     insert_activity_loader_prv(Lampa.Storage.get('ani_load'));
                 }
-                // Оновлюємо стилі модального вікна при зміні кольору або стану плагіна
+                // Оновлюємо стилі модального вікна
                 if (document.getElementById('aniload')) {
                     create_ani_modal();
                     var modal = document.querySelector('.ani_modal_root');
@@ -380,6 +380,10 @@
                                            '</div>';
                         modal.innerHTML = $('<div>' + topRowHtml + modalContent + '</div>').find('.ani_modal_root').html();
                     }
+                }
+                // Оновлюємо стилі для всіх елементів
+                if (Lampa.Storage.get('ani_active')) {
+                    insert_activity_loader_prv(Lampa.Storage.get('ani_load', './img/loader.svg'));
                 }
             }
         });
@@ -464,11 +468,13 @@
             }
         }, 500);
 
-        // Застосовуємо збережений завантажувач, якщо він активний
+        // Застосовуємо збережений завантажувач і стилі при ініціалізації
         if (Lampa.Storage.get('ani_load') && Lampa.Storage.get('ani_active') && Lampa.Storage.get('ani_load') !== './img/loader.svg') {
             setCustomLoader(Lampa.Storage.get('ani_load'));
             insert_activity_loader_prv(Lampa.Storage.get('ani_load'));
         }
+        // Ініціалізуємо стилі модального вікна
+        create_ani_modal();
     }
 
     // Функція для застосування налаштувань за темою
@@ -477,6 +483,8 @@
             setCustomLoader(Lampa.Storage.get('ani_load'));
             insert_activity_loader_prv(Lampa.Storage.get('ani_load'));
         }
+        // Оновлюємо стилі модального вікна
+        create_ani_modal();
     }
 
     // Запускаємо плагін після готовності програми
