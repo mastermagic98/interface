a4b680a59ad177cb556dfe7baec76482fa6c4b61
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-10-29 17:18:04 +0200

    Update animations.js

--- animations.js
+++ animations.js
@@ -12,34 +12,34 @@
     var animEnabled = localStorage.getItem('animations') !== 'false';
 
     function toggleAnimations() {
-        var style = $('#animations_style');
+        var styleId = 'animations_style';
+        $('#' + styleId).remove();
+
         if (animEnabled) {
-            if (!style.length) {
-                var css = "\n<style id=\"animations_style\">\n" +
-                    ".card{transform:scale(1);transition:transform .3s ease}\n" +
-                    ".card.focus{transform:scale(1.03)}\n" +
-                    ".torrent-item,.online-prestige{transform:scale(1);transition:transform .3s ease}\n" +
-                    ".torrent-item.focus,.online-prestige.focus{transform:scale(1.01)}\n" +
-                    ".extensions__item,.extensions__block-add,.full-review-add,.full-review,.tag-count,.full-person,.full-episode,.simple-button,.full-start__button,.items-cards .selector,.card-more,.explorer-card__head-img.selector,.card-episode{transform:scale(1);transition:transform .3s ease}\n" +
-                    ".extensions__item.focus,.extensions__block-add.focus,.full-review-add.focus,.full-review.focus,.tag-count.focus,.full-person.focus,.full-episode.focus,.simple-button.focus,.full-start__button.focus,.items-cards .selector.focus,.card-more.focus,.explorer-card__head-img.selector.focus,.card-episode.focus{transform:scale(1.03)}\n" +
-                    ".menu__item{transition:transform .3s ease}\n" +
-                    ".menu__item.focus{transform:translateX(-.2em)}\n" +
-                    ".selectbox-item,.settings-folder,.settings-param{transition:transform .3s ease}\n" +
-                    ".selectbox-item.focus,.settings-folder.focus,.settings-param.focus{transform:translateX(.2em)}\n" +
-                    "</style>";
-                $('head').append(css);
-            }
-        } else {
-            style.remove();
+            var css = '<style id="' + styleId + '">' +
+                '.card{transform:scale(1);transition:transform .3s ease}' +
+                '.card.focus{transform:scale(1.03)}' +
+                '.torrent-item,.online-prestige{transform:scale(1);transition:transform .3s ease}' +
+                '.torrent-item.focus,.online-prestige.focus{transform:scale(1.01)}' +
+                '.extensions__item,.extensions__block-add,.full-review-add,.full-review,.tag-count,.full-person,.full-episode,.simple-button,.full-start__button,.items-cards .selector,.card-more,.explorer-card__head-img.selector,.card-episode{transform:scale(1);transition:transform .3s ease}' +
+                '.extensions__item.focus,.extensions__block-add.focus,.full-review-add.focus,.full-review.focus,.tag-count.focus,.full-person.focus,.full-episode.focus,.simple-button.focus,.full-start__button.focus,.items-cards .selector.focus,.card-more.focus,.explorer-card__head-img.selector.focus,.card-episode.focus{transform:scale(1.03)}' +
+                '.menu__item{transition:transform .3s ease}' +
+                '.menu__item.focus{transform:translateX(-.2em)}' +
+                '.selectbox-item,.settings-folder,.settings-param{transition:transform .3s ease}' +
+                '.selectbox-item.focus,.settings-folder.focus,.settings-param.focus{transform:translateX(.2em)}' +
+                '</style>';
+            $('head').append(css);
         }
     }
 
     function startPlugin() {
-        if (!localStorage.getItem('animations')) {
+        if (localStorage.getItem('animations') === null) {
             localStorage.setItem('animations', 'true');
+        } else {
+            animEnabled = localStorage.getItem('animations') === 'true';
         }
 
-        // Додаємо пункт у вже існуюче меню "Налаштування кольору акценту"
+        // Додаємо пункт у меню "Налаштування кольору акценту"
         Lampa.SettingsApi.addParam({
             component: 'accent_color_plugin',
             param: {
@@ -65,7 +65,9 @@
         startPlugin();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
+            if (e.type === 'ready') {
+                startPlugin();
+            }
         });
     }
 
