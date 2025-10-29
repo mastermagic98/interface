efc25f9efeb30434d1cfcbd27db0a6aa940d1407
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-10-29 11:01:11 +0200

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,7 +1,7 @@
 component: 'accent_color_plugin'
-(function() {
+(function () {
     'use strict';
-    
+
     Lampa.Lang.add({
         maxsm_animations: {
             ru: "Анимации",
@@ -9,38 +9,38 @@ component: 'accent_color_plugin'
             uk: "Анімації"
         }
     });
-    
-    function animations() {
+
+    var animations = function () {
         var enabled = localStorage.getItem('maxsm_animations') === 'true';
         $('#maxsm_animations').remove();
         if (enabled) {
-            var style = "\n<style id=\"maxsm_animations\">\n" +
-                ".card{transform:scale(1);transition:transform .3s ease}\n" +
-                ".card.focus{transform:scale(1.03)}\n" +
-                ".torrent-item,.online-prestige{transform:scale(1);transition:transform .3s ease}\n" +
-                ".torrent-item.focus,.online-prestige.focus{transform:scale(1.01)}\n" +
-                ".extensions__item,.extensions__block-add,.full-review-add,.full-review,.tag-count,.full-person,.full-episode,.simple-button,.full-start__button,.items-cards .selector,.card-more,.explorer-card__head-img.selector,.card-episode{transform:scale(1);transition:transform .3s ease}\n" +
-                ".extensions__item.focus,.extensions__block-add.focus,.full-review-add.focus,.full-review.focus,.tag-count.focus,.full-person.focus,.full-episode.focus,.simple-button.focus,.full-start__button.focus,.items-cards .selector.focus,.card-more.focus,.explorer-card__head-img.selector.focus,.card-episode.focus{transform:scale(1.03)}\n" +
-                ".menu__item{transition:transform .3s ease}\n" +
-                ".menu__item.focus{transform:translateX(-.2em)}\n" +
-                ".selectbox-item,.settings-folder,.settings-param{transition:transform .3s ease}\n" +
-                ".selectbox-item.focus,.settings-folder.focus,.settings-param.focus{transform:translateX(.2em)}\n" +
-                "</style>\n";
+            var style = "<style id=\"maxsm_animations\">" +
+                ".card{transform:scale(1);transition:transform .3s ease}" +
+                ".card.focus{transform:scale(1.03)}" +
+                ".torrent-item,.online-prestige{transform:scale(1);transition:transform .3s ease}" +
+                ".torrent-item.focus,.online-prestige.focus{transform:scale(1.01)}" +
+                ".extensions__item,.extensions__block-add,.full-review-add,.full-review,.tag-count,.full-person,.full-episode,.simple-button,.full-start__button,.items-cards .selector,.card-more,.explorer-card__head-img.selector,.card-episode{transform:scale(1);transition:transform .3s ease}" +
+                ".extensions__item.focus,.extensions__block-add.focus,.full-review-add.focus,.full-review.focus,.tag-count.focus,.full-person.focus,.full-episode.focus,.simple-button.focus,.full-start__button.focus,.items-cards .selector.focus,.card-more.focus,.explorer-card__head-img.selector.focus,.card-episode.focus{transform:scale(1.03)}" +
+                ".menu__item{transition:transform .3s ease}" +
+                ".menu__item.focus{transform:translateX(-.2em)}" +
+                ".selectbox-item,.settings-folder,.settings-param{transition:transform .3s ease}" +
+                ".selectbox-item.focus,.settings-folder.focus,.settings-param.focus{transform:translateX(.2em)}" +
+                "</style>";
             $('body').append(style);
         }
-    }
-    
-    function startPlugin() {
+    };
+
+    var startPlugin = function () {
         if (!localStorage.getItem('maxsm_animations')) {
             localStorage.setItem('maxsm_animations', 'true');
         }
-        
+
         Lampa.SettingsApi.addComponent({
             component: "accent_color_plugin",
             name: Lampa.Lang.translate('maxsm_animations'),
             icon: ''
         });
-        
+
         Lampa.SettingsApi.addParam({
             component: 'accent_color_plugin',
             param: {
@@ -52,19 +52,21 @@ component: 'accent_color_plugin'
                 name: Lampa.Lang.translate('maxsm_animations'),
                 description: ''
             },
-            onChange: function() {
+            onChange: function () {
                 animations();
             }
         });
-        
+
         animations();
-    }
-    
+    };
+
     if (window.appready) {
         startPlugin();
     } else {
-        Lampa.Listener.follow('app', function(e) {
-            if (e.type == 'ready') startPlugin();
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type === 'ready') {
+                startPlugin();
+            }
         });
     }
 })();
