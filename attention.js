058d7d0bb0b52c893f13eb84e8b00d9dee2b4b85
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2025-12-19 15:06:24 +0200

    Update attention.js

--- attention.js
+++ attention.js
@@ -165,7 +165,7 @@
         });  
     }  
   
-    // Додаємо параметр до налаштувань  
+// Додаємо параметр до налаштувань  
 function addSettingsParam() {    
     Lampa.SettingsApi.addParam({    
         component: 'interface_customization',    
@@ -175,7 +175,15 @@ function addSettingsParam() {
             default: true    
         },    
         field: {    
-            name: '<span class="settings-param-icon"><svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg"><path d="M7 5v3"/><circle cx="7" cy="11" r=".5"/><path d="M7.89 1.05a1 1 0 0 0-1.78 0l-5.5 11a1 1 0 0 0 .89 1.45h11a1 1 0 0 0 .89-1.45Z"/></svg></span>' + Lampa.Lang.translate('attention_warnings_title')    
+            name: '<div class="settings-folder-param selector">' +  
+                  '<div class="settings-folder-param__icon">' +  
+                  '<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">' +  
+                  '<path d="M7 5v3"/><circle cx="7" cy="11" r=".5"/>' +  
+                  '<path d="M7.89 1.05a1 1 0 0 0-1.78 0l-5.5 11a1 1 0 0 0 .89 1.45h11a1 1 0 0 0 .89-1.45Z"/>' +  
+                  '</svg>' +  
+                  '</div>' +  
+                  '<div class="settings-folder-param__name">' + Lampa.Lang.translate('attention_warnings_title') + '</div>' +  
+                  '</div>'  
         }    
     });    
 }
