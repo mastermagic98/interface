5182a155ec429603ab68b6fa50569e3b581e622f
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-01-09 17:15:58 +0200

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -680,7 +680,7 @@
                 component: 'interface_customization',
                 param: { name: 'buttons_editor_enabled', type: 'trigger', default: true },
                 field: {
-                    name: '<div style="display: flex; align-items: center;"><svg width="200" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" style="margin-right:10px;flex-shrink:0;min-width:24px;min-height:24px;max-width:24px;max-height:24px" fill="none"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="m7.5 9l-3 .54L5 6.5L10.73.79a1 1 0 0 1 1.42 0l1.06 1.06a1 1 0 0 1 0 1.42Z"/><path d="M12 9.5v3a1 1 0 0 1-1 1H1.5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3"/></g></svg>' + t('custom_interface_plugin_button_editor') + '</div>',
+                    name: '<div style="display: flex; align-items: center;"><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 14" style="margin-right:10px;flex-shrink:0;min-width:24px;min-height:24px;max-width:24px;max-height:24px" fill="none"><g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"><path d="m7.5 9-3 .54L5 6.5 10.73.79a1 1 0 0 1 1.42 0l1.06 1.06a1 1 0 0 1 0 1.42Z"/><path d="M12 9.5v3a1 1 0 0 1-1 1H1.5a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3"/></g></svg>' + t('custom_interface_plugin_button_editor') + '</div>',
                     description: t('custom_interface_plugin_button_editor_desc')
                 },
                 onChange: function(value) {
