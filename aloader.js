133d617bb64346c45e93737f2e60c9960e0fcd90
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-01-09 14:17:47 +0200

    Update aloader.js

--- aloader.js
+++ aloader.js
@@ -193,26 +193,25 @@
                 field: {  
                     name:
     '<div style="display:flex;align-items:center;">' +
-        '<svg width="32" height="32" viewBox="0 0 32 32" ' +
-             'fill="currentColor" xmlns="http://www.w3.org/2000/svg" ' +
-             'style="margin-right:10px;flex-shrink:0;min-width:32px;min-height:32px;max-width:32px;max-height:32px;">' +
-
-            '<circle cx="16" cy="16" r="3"/>' +
-            '<g>' +
-                '<circle cx="8" cy="16" r="3"/>' +
-                '<circle cx="24" cy="16" r="3"/>' +
-                '<animateTransform ' +
-                    'attributeName="transform" ' +
-                    'type="rotate" ' +
-                    'calcMode="spline" ' +
-                    'dur="1s" ' +
-                    'keySplines=".36,.6,.31,1;.36,.6,.31,1" ' +
-                    'values="0 16 16;180 16 16;360 16 16" ' +
-                    'repeatCount="indefinite"/>' +
-            '</g>' +
-        '</svg>' +
-        Lampa.Lang.translate('params_ani_name') +
-    '</div>',  
+  '<svg xmlns="http://www.w3.org/2000/svg" ' +
+       'width="32" height="32" viewBox="0 0 32 32" fill="currentColor" ' +
+       'style="margin-right:10px;flex-shrink:0;min-width:32px;min-height:32px;max-width:32px;max-height:32px">' +
+    '<circle cx="16" cy="16" r="3"/>' +
+    '<g>' +
+      '<circle cx="8" cy="16" r="3"/>' +
+      '<circle cx="24" cy="16" r="3"/>' +
+      '<animateTransform ' +
+        'attributeName="transform" ' +
+        'type="rotate" ' +
+        'calcMode="spline" ' +
+        'dur="1s" ' +
+        'keySplines=".36,.6,.31,1;.36,.6,.31,1" ' +
+        'values="0 16 16;180 16 16;360 16 16" ' +
+        'repeatCount="indefinite"/>' +
+    '</g>' +
+  '</svg>' +
+  Lampa.Lang.translate('params_ani_name') +
+'</div>',  
                     description: Lampa.Lang.translate('params_ani_desc')  
                 },  
                 onChange: function (value) {  
@@ -252,17 +251,17 @@
                 field: {  
                    name:
     '<div style="display:flex;align-items:center;">' +
-        '<div style="' +
-            'margin-right:10px;' +
-            'flex-shrink:0;' +
-            'width:32px;height:32px;' +
-            'min-width:32px;min-height:32px;' +
-            'max-width:32px;max-height:32px;' +
-            'display:flex;align-items:center;justify-content:center;">' +
-            '<div class="activity__loader_prv"></div>' +
-        '</div>' +
-        Lampa.Lang.translate('params_ani_select') +
-    '</div>'
+  '<div style="' +
+    'width:32px;height:32px;' +
+    'min-width:32px;min-height:32px;' +
+    'max-width:32px;max-height:32px;' +
+    'margin-right:10px;' +
+    'flex-shrink:0;' +
+    'display:flex;align-items:center;justify-content:center;">' +
+      '<div class="activity__loader_prv"></div>' +
+  '</div>' +
+  Lampa.Lang.translate('params_ani_select') +
+'</div>'
                 },  
                 onRender: function (item) {  
                     if (!Lampa.Storage.get('ani_active')) {  
