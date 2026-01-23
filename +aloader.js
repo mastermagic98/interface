92af4e46a1a7b76abc942ccd57b34415eb6ccfc5
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-01-23 14:13:29 +0200

    Implement focus and blur listeners for settings parameter
    Add focus and blur event listeners to the previous element in settings.

--- +aloader.js
+++ +aloader.js
@@ -178,7 +178,17 @@
     function isValidSvgUrl(url) {
         return /^https?:\/\/.*\.svg$/.test(url);
     }
-
+function addPrvFocusListener() {  
+    var prvElement = document.querySelector('.settings-param[data-name="select_ani_mation"] .activity__loader_prv');  
+    if (prvElement) {  
+        prvElement.addEventListener('focus', function() {  
+            this.style.outline = '2px solid var(--main-color)';  
+        });  
+        prvElement.addEventListener('blur', function() {  
+            this.style.outline = 'none';  
+        });  
+    }  
+}  
     function aniLoad() {
         var icon_plugin = '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><circle cx="12" cy="12" r="3"/><g><circle cx="4" cy="12" r="3"/><circle cx="20" cy="12" r="3"/><animateTransform attributeName="transform" type="rotate" calcMode="spline" dur="1s" keySplines=".36,.6,.31,1;.36,.6,.31,1" values="0 12 12;180 12 12;360 12 12" repeatCount="indefinite"/></g></svg>';
         try {
