ba46b9c2f5a1776459094f7edc00d164a4c02aa0
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-02-19 13:43:44 +0200

    Refactor settings addition and initialization logic

--- key_off.js
+++ key_off.js
@@ -127,66 +127,90 @@
         });
     }
 
-    // Додаємо компонент у Налаштування
-    Lampa.SettingsApi.addComponent({
-        component: 'keyboard_multi_hide_plugin',
-        name: 'Клавіатура',
-        icon: '<svg fill="#fff" width="38px" height="38px" viewBox="0 0 24 24"><path d="M20 5H4a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3Zm1 11a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v8Zm-6-3H9a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2Zm3.5-4h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2Z"/></svg>'
-    });
-
-    // Додаємо параметр для вибору default мови
-    Lampa.SettingsApi.addParam({
-        component: 'keyboard_multi_hide_plugin',
-        param: {
-            name: 'default_keyboard_lang',
-            type: 'trigger',
-            default: false
-        },
-        field: {
-            name: 'Мова за замовчуванням',
-            description: 'Виберіть мову клавіатури за замовчуванням'
-        },
-        onRender(el) {
-            el.off('hover:enter').on('hover:enter', function () {
-                openDefaultLangMenu();
-            });
+    // Функція додавання налаштувань з логуванням
+    function addSettings() {
+        if (!Lampa.SettingsApi) {
+            console.log('Keyboard Plugin: SettingsApi not available');
+            return;
         }
-    });
 
-    // Додаємо параметр для вимкнення мов
-    Lampa.SettingsApi.addParam({
-        component: 'keyboard_multi_hide_plugin',
-        param: {
-            name: 'select_keyboard_hide',
-            type: 'trigger',
-            default: false
-        },
-        field: {
-            name: 'Вимкнути мови',
-            description: 'Вимкніть непотрібні мови клавіатури'
-        },
-        onRender(el) {
-            el.off('hover:enter').on('hover:enter', function () {
-                Lampa.Select.show({
-                    title: 'Вимкнути мови?',
-                    items: [
-                        { title: 'Так', selected: true },
-                        { title: 'Ні' }
-                    ],
-                    onSelect: function (item) {
-                        if (item.title === 'Так') {
-                            openHideMenu();
-                        }
-                    },
-                    onBack: function () {
-                        Lampa.Controller.toggle('settings_component');
-                    }
+        // Додаємо компонент у Налаштування
+        Lampa.SettingsApi.addComponent({
+            component: 'keyboard_multi_hide_plugin',
+            name: 'Клавіатура',
+            icon: '<svg fill="#fff" width="38px" height="38px" viewBox="0 0 24 24"><path d="M20 5H4a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3Zm1 11a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v8Zm-6-3H9a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2Zm3.5-4h-1a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2Z"/></svg>'
+        });
+        console.log('Keyboard Plugin: Added component keyboard_multi_hide_plugin');
+
+        // Додаємо параметр для вибору default мови
+        Lampa.SettingsApi.addParam({
+            component: 'keyboard_multi_hide_plugin',
+            param: {
+                name: 'default_keyboard_lang',
+                type: 'trigger',
+                default: false
+            },
+            field: {
+                name: 'Мова за замовчуванням',
+                description: 'Виберіть мову клавіатури за замовчуванням'
+            },
+            onRender(el) {
+                el.off('hover:enter').on('hover:enter', function () {
+                    openDefaultLangMenu();
                 });
-            });
-        }
-    });
+            }
+        });
+        console.log('Keyboard Plugin: Added param default_keyboard_lang');
 
-    // Застосування приховування при старті
-    Lampa.Listener.follow('full', e => e.type === 'start' && setTimeout(applyHiding, 300));
-    if (window.appready) setTimeout(applyHiding, 500);
+        // Додаємо параметр для вимкнення мов
+        Lampa.SettingsApi.addParam({
+            component: 'keyboard_multi_hide_plugin',
+            param: {
+                name: 'select_keyboard_hide',
+                type: 'trigger',
+                default: false
+            },
+            field: {
+                name: 'Вимкнути мови',
+                description: 'Вимкніть непотрібні мови клавіатури'
+            },
+            onRender(el) {
+                el.off('hover:enter').on('hover:enter', function () {
+                    Lampa.Select.show({
+                        title: 'Вимкнути мови?',
+                        items: [
+                            { title: 'Так', selected: true },
+                            { title: 'Ні' }
+                        ],
+                        onSelect: function (item) {
+                            if (item.title === 'Так') {
+                                openHideMenu();
+                            }
+                        },
+                        onBack: function () {
+                            Lampa.Controller.toggle('settings_component');
+                        }
+                    });
+                });
+            }
+        });
+        console.log('Keyboard Plugin: Added param select_keyboard_hide');
+    }
+
+    // Застосування приховування та додавання налаштувань при старті
+    if (window.appready) {
+        setTimeout(() => {
+            addSettings();
+            applyHiding();
+        }, 500);
+    } else {
+        Lampa.Listener.follow('app', e => {
+            if (e.type === 'ready') {
+                setTimeout(() => {
+                    addSettings();
+                    applyHiding();
+                }, 300);
+            }
+        });
+    }
 })();
