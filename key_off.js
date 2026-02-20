21e260850bbe58936063167fb04f0e5a2eb1ea8d
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-02-20 12:32:25 +0200

    Update key_off.js

--- key_off.js
+++ key_off.js
@@ -2,8 +2,8 @@
     'use strict';
 
     if (!Lampa.Manifest || Lampa.Manifest.app_digital < 300) return;
-    if (window.keyboard_settings_v6) return;
-    window.keyboard_settings_v6 = true;
+    if (window.keyboard_settings_v7) return;
+    window.keyboard_settings_v7 = true;
 
     const LANGUAGES = [
         { title: 'Українська', code: 'uk', hideKey: 'keyboard_hide_uk' },
@@ -31,29 +31,29 @@
     function applySettings() {
         try {
             const defaultCode = getDefaultCode();
-            console.log('[Keyboard v6] Default:', getDefaultTitle(), '(' + defaultCode + ')');
-            console.log('[Keyboard v6] Hide values in Storage:', LANGUAGES.map(l => l.hideKey + ': ' + Lampa.Storage.get(l.hideKey, 'false')));
+            console.log('[Keyboard v7] Default:', getDefaultTitle(), '(' + defaultCode + ')');
+            console.log('[Keyboard v7] Hide values:', LANGUAGES.map(l => l.title + ': ' + Lampa.Storage.get(l.hideKey, 'false')));
 
             LANGUAGES.forEach(function(lang) {
                 const hide = Lampa.Storage.get(lang.hideKey, 'false') === 'true';
                 const shouldHide = hide && lang.code !== defaultCode;
 
-                const element = $('.selectbox-item.selector > div.selectbox-item__text:contains("' + lang.title + '")');
+                const element = $('.selectbox-item.selector > div:contains("' + lang.title + '")');
                 if (element.length > 0) {
-                    const parent = element.parent();
+                    const parent = element.parent('div');
                     if (shouldHide) {
                         parent.hide();
-                        console.log('[Keyboard v6] СХОВАНО:', lang.title);
+                        console.log('[Keyboard v7] СХОВАНО:', lang.title);
                     } else {
                         parent.show();
-                        console.log('[Keyboard v6] ПОКАЗАНО:', lang.title);
+                        console.log('[Keyboard v7] ПОКАЗАНО:', lang.title);
                     }
                 } else {
-                    console.log('[Keyboard v6] Не знайдено елемент для:', lang.title);
+                    console.log('[Keyboard v7] Не знайдено елемент для:', lang.title);
                 }
             });
         } catch(e) {
-            console.log('[Keyboard v6] Помилка applySettings:', e.message);
+            console.log('[Keyboard v7] Помилка applySettings:', e.message);
         }
     }
 
@@ -76,9 +76,8 @@
                     const langObj = LANGUAGES.find(l => l.code === item.value);
                     if (langObj) Lampa.Storage.set(langObj.hideKey, 'false');
                     Lampa.Storage.set('keyboard_default_lang', item.value);
-                    applySettings();
                     setTimeout(applySettings, 200);
-                    console.log('[Keyboard v6] Default змінено на:', item.title);
+                    console.log('[Keyboard v7] Default змінено на:', item.title);
                 }
             },
             onBack: function() {
@@ -110,9 +109,8 @@
                 const current = Lampa.Storage.get(item.key, 'false') === 'true';
                 const newVal = current ? 'false' : 'true';
                 Lampa.Storage.set(item.key, newVal);
-                console.log('[Keyboard v6] Змінено ' + item.key + ' на ' + newVal);
+                console.log('[Keyboard v7] Змінено ' + item.key + ' на ' + newVal);
 
-                applySettings();
                 setTimeout(applySettings, 200);
                 setTimeout(openHideMenu, 100);
             },
@@ -124,14 +122,14 @@
 
     // Додаємо компонент
     Lampa.SettingsApi.addComponent({
-        component: 'keyboard_settings_v6',
+        component: 'keyboard_settings_v7',
         name: 'Налаштування клавіатури',
         icon: '<svg fill="#fff" width="38px" height="38px" viewBox="0 0 24 24"><path d="M20 5H4a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h16a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3Z"/></svg>'
     });
 
-    // Параметр default - trigger с заменой текста
+    // Параметр default - trigger з заміною 'Так'
     Lampa.SettingsApi.addParam({
-        component: 'keyboard_settings_v6',
+        component: 'keyboard_settings_v7',
         param: {
             name: 'keyboard_default_trigger',
             type: 'trigger',
@@ -146,7 +144,7 @@
                 const currentTitle = getDefaultTitle();
                 el.find('.settings-param__value').text(currentTitle);
             } catch(e) {
-                console.log('[Keyboard v6] Помилка onRender default:', e.message);
+                console.log('[Keyboard v7] Помилка onRender default:', e.message);
             }
             el.off('hover:enter').on('hover:enter', openDefaultMenu);
         }
@@ -154,7 +152,7 @@
 
     // Параметр hide - trigger
     Lampa.SettingsApi.addParam({
-        component: 'keyboard_settings_v6',
+        component: 'keyboard_settings_v7',
         param: {
             name: 'keyboard_hide_trigger',
             type: 'trigger',
@@ -169,18 +167,25 @@
         }
     });
 
-    // Інтервал
+    // Інтервал тільки для перевірки кнопки LANG
     setInterval(function() {
         if ($('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded').length > 0) {
-            applySettings();
+            setTimeout(applySettings, 100);
         }
-    }, 0);
+    }, 50);
+
+    // Додаємо listener на клік по кнопці LANG
+    function setupListeners() {
+        $('body').on('click', '.hg-button.hg-functionBtn.hg-button-LANG.selector.binded', function() {
+            setTimeout(applySettings, 50);
+            setTimeout(applySettings, 200);
+            console.log('[Keyboard v7] Натиснуто LANG - застосовую hiding');
+        });
+    }
 
     // Запуск
     function start() {
-        applySettings();
-        setTimeout(applySettings, 500);
-        setTimeout(applySettings, 1200);
+        setupListeners();
     }
 
     if (window.appready) start();
@@ -189,14 +194,13 @@
     });
 
     Lampa.Listener.follow('full', function(e) {
-        if (e.type === 'start') setTimeout(start, 1500);
+        if (e.type === 'start') setTimeout(setupListeners, 1500);
     });
 
     Lampa.Listener.follow('select', function(e) {
         if (e.type === 'open') {
             setTimeout(applySettings, 100);
             setTimeout(applySettings, 300);
-            setTimeout(applySettings, 600);
         }
     });
 
