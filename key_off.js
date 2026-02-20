c2f7e70fc965997e4338c2e1069f0bc586fd16b1
mastermagic98 <87490408+mastermagic98@users.noreply.github.com>
2026-02-20 11:00:07 +0200

    Update key_off.js

--- key_off.js
+++ key_off.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
 	
-function add() {
+/*function add() {
   Lampa.Storage.set('keyboard_default_lang', 'Українська')
   var elementUA = $('.selectbox-item.selector > div:contains("Русский")');
   if(elementUA.length > 0) elementUA.parent('div').hide();
@@ -9,7 +9,7 @@ function add() {
   var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
   if(elementXZ.length > 0) elementXZ.parent('div').hide();
 }
-/*
+*/
 setInterval(function() {
   var elementCHlang = $('div.hg-button.hg-functionBtn.hg-button-LANG.selector.binded')
   if (elementCHlang.length > 0){
@@ -20,7 +20,7 @@ setInterval(function() {
     var elementXZ = $('.selectbox-item.selector > div:contains("עִברִית")');
     if(elementXZ.length > 0) elementXZ.parent('div').hide();
   }
- }, 0)*/
+ }, 0)
 
 if(window.appready) add();
   else {
